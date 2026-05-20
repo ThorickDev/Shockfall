00ff46ac06c4f225761ccd71188993aeef2ac655
RichardBatista <richardbatista@on.br>
2026-05-20 17:55:39 -0300

    feat: implamentation scene reference

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+
+    public float DelayDetonation = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+}
